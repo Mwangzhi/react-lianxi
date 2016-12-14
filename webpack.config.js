afeea2ff860bc46129b59326498ca39d53112ab5
Mwangzhi <519319609@qq.com>
2016-12-14 23:57:43 +0800

    a

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var htmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+module.exports = {
+    entry:'./src/index.js',
+    output:{
+        path:'./build',
+        filename:'bundle.js'
+    },
+    devServer:{
+    	contentBase:'build',
+    	inline:true,
+    	port:3030,
+    	status:{
+    		colors:true
+    	}
+    },
+    module:{
+    	loaders:[
+    		{
+    			test:/\.js$/,
+    			loader:'babel',
+    			exclude:/node_modules/
+    		},
+            {
+                test:/\.css$/,
+                loader:'style!css',
+                include:path.resolve(__dirname,'src')
+            }
+            ,
+            {
+                test:/\.less$/,
+                loader:'style!css!less',
+                include:path.resolve(__dirname,'src')
+            }
+    	]
+    },
+
+    plugins:[
+     new htmlWebpackPlugin({
+     	title:'搭建前端工作流',
+     	template:'./src/index.html'
+     }) 
+    ]
+}
